92b1fd5fca6570713674b9e455b6c1335496e552
stsluciano <112117221+stsluciano@users.noreply.github.com>
2025-06-26 14:39:36 -0300

    Update legibilidade.c

--- pset2/legibilidade/legibilidade.c
+++ pset2/legibilidade/legibilidade.c
@@ -1,1 +1,54 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
 
+int main(void)
+{
+    string text = get_string("Text: ")
+
+    int letters = 0;
+    int words = 1; //Começar com 1 já que a última palavra não terá um espaço após isso
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text);++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        else if (text[i] == '')
+        {
+            words++;
+        }
+        else if (text[i]=='.' || text[i]=='!' || text[i]=='?')
+        {
+            sentences++;
+        }
+    }
+    float L=(float) letters/words*100;
+    float S=(float) sentences/words*100;
+
+    float index = 0.0588*L-0.296*S-15.8;
+
+    int grade=round(index);
+
+    if(grade<1)
+    {
+        printf("Before Grade 1/n");
+    }
+
+    else if(grade>=16)
+    {
+        printf(Grade 16+/n");
+    }
+
+    else
+
+     {
+       printf("Grade%i/n", grade);
+     }
+
+    }
